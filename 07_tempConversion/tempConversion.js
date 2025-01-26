89d24547d18fba6b9074349d4772d50331200b33
mflojo <m.eric.flojo@gmail.com>
2025-01-25 19:00:35 -0800

    Add solution for conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let calc = ((fahrenheit-32) * (5/9));
+  if (Number.isInteger(calc)) {
+    return calc;
+  } else {
+    return parseFloat(calc.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let calc = ((celsius*(9/5) + 32));
+  if (Number.isInteger(calc)) {
+    return calc;
+  } else {
+    return parseFloat(calc.toFixed(1));
+  }
 };
 
 // Do not edit below this line
