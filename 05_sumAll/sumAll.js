60d0c6cc82602c7b8fa68a4babd145142ae891d0
mflojo <m.eric.flojo@gmail.com>
2025-01-25 18:29:05 -0800

    Add function to sum all with given input

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(numStart, numEnd) {
+    let finalSum = 0;
+    if (isNaN(numStart) || isNaN(numEnd)) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(numStart) || !Number.isInteger(numEnd)) {
+        return "ERROR";
+    }
+    if (numStart<0 || numEnd<0) {
+        return "ERROR";
+    }
+    if (numStart === numEnd) {
+        return numStart;
+    }
+    if (numStart > numEnd) {
+        let temp = numStart;
+        numStart = numEnd;
+        numEnd = temp;
+    }
+    for (let i=numStart; i<=numEnd; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
